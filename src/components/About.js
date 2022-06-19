1e9da395c753eb6e2ac2b7472a68ec65c4cfe5db
Tyler Beck <softwaretpd2035@gmail.com>
2022-06-19 12:35:18 -0400

    fixed spacing

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,6 @@ export default function About() {
   return (
     <div className='About'>
       <h2 className='About-h2'> About </h2> 
-      <p className='About-p'> I am a full stack developer who is highly flexible, established learner, who is continuously expanding my knowledge to maintain current and upcoming coding compliances. I am looking to bring my skills to a tech company with global reach. </p>
+      <p className='About-p'> I am a full stack developer who is highly flexible, established learner, who is continuously expanding my knowledge! I am looking to bring my skills to a tech company with global reach. </p>
     </div>  
 )}
